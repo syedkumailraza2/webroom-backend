af1b3a9625ad970536e4555ab5263986943eef7c
Shadab-2604 <138616478+Shadab-2604@users.noreply.github.com>
2025-02-28 10:58:06 +0530

    added route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from 'cors'
 import connectDB from "./config/db.js"
 import dotenv from "dotenv";
+import studyMaterialRoutes from "./Routes/studymaterial.route.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 8000
@@ -10,6 +11,8 @@ const app = express()
 app.get('/', (req,res)=>{
     res.send('Hello World!')
 })
+
+app.use("/notes", studyMaterialRoutes);
 
 connectDB()
 
